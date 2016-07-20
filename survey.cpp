8f4d1536657d36013e6345c9dd3100c04bfe1c39
Gene Selkov <selkovjr@gmail.com>
2016-07-19 22:39:41 -0700

    fixed the cols/rows mix-up and added proper distortion mask

--- survey.cpp
+++ survey.cpp
@@ -29,16 +29,29 @@ using namespace cv;
 using namespace termcolor;
 
 bool verbose;
+bool exr_mode;
+
 int run = 0;
 
-Mat ref_image, work_plane;
+Mat ref_plane, work_plane;
+Mat blank, mask;
 Mat dst, map_x, map_y;
 
 #define RED 0
 #define GREEN 1
 #define BLUE 2
 #define UNKNOWN 3
+
+#define CFA_WIDTH 3264
+#define CFA_HEIGHT 2464
+
 int work_plane_color = UNKNOWN;
+
+string to_string(double v) {
+  stringstream ss;
+  ss << v;
+  return ss.str();
+}
 
 void color_switch(ostream& s, int color) {
   switch (color) {
@@ -64,45 +77,75 @@ double diff (double a, double b, double c, double d) {
   clock_t start_time = clock(), end_time;
   double elapsed;
 
+  // Fill the CFA area with white
+  if (verbose) cerr << right << setw(5) << run << reset << lightgrey <<  ": making mask ... " << reset;
+  blank.create( work_plane.size(), work_plane.type() );
+#pragma omp parallel shared(blank, exr_mode) private(i, j)
+  {
+#pragma omp for collapse(2)
+    for (j = 0; j < blank.rows; j++ ) {
+      for (i = 0; i < blank.cols; i++ ) {
+        if (exr_mode) {
+          if ( // CFA interior
+            i + j >= CFA_WIDTH - 1 &&                   // NW boundary
+            j > i - CFA_WIDTH - 1 &&                    // NE boundary
+            i + j < CFA_WIDTH + 2 * CFA_HEIGHT - 1 &&   // SE boundary
+            i > j - CFA_WIDTH                           // SW boundary
+          ) {
+            blank.at<ushort>(j,i) = 65535;
+          }
+          else {
+            blank.at<ushort>(j,i) = j;
+          }
+        }
+        else {
+          blank.at<ushort>(j,i) = 65535;
+        }
+      }
+    }
+  } /* end of parallel section */
+
+
   /// Create dst, map_x and map_y with the same size as work_plane:
   dst.create( work_plane.size(), work_plane.type() );
   map_x.create( work_plane.size(), CV_32FC1 );
   map_y.create( work_plane.size(), CV_32FC1 );
 
   run++;
-  if (verbose) cerr << right << setw(5) << run << reset << lightgrey <<  ": computing maps ... " << reset;
+  if (verbose) cerr << lightgrey <<  "computing maps ... " << reset;
 #pragma omp parallel shared(map_x, map_y, a, b, c, d) private(i, j, x, y, r, rd)
   {
 #pragma omp for collapse(2)
     for (j = 0; j < work_plane.rows; j++ ) {
       for (i = 0; i < work_plane.cols; i++ ) {
-        if (work_plane.at<ushort>(i,j) > 0) { // don't move masked pixels (black outer triangles)
-          x = (double)(2 * i - work_plane.rows) / work_plane.rows;
-          y = (double)(2 * j - work_plane.cols) / work_plane.cols;
+        if (blank.at<ushort>(j, i) == 65535) { // don't move masked pixels (black outer triangles)
+          x = (double)(2 * i - work_plane.cols) / work_plane.cols;
+          y = (double)(2 * j - work_plane.rows) / work_plane.rows;
           r = sqrt(x * x + y * y);
           rd = a * r + b * pow(r, 2) + c * pow(r, 3) + d * pow(r, 4);
-          map_x.at<float>(j,i) = (x * rd / r + 1) * work_plane.rows / 2;
-          map_y.at<float>(j,i) = (y * rd / r + 1) * work_plane.cols / 2;
+          map_x.at<float>(j, i) = (x * rd / r + 1) * work_plane.cols / 2;
+          map_y.at<float>(j, i) = (y * rd / r + 1) * work_plane.rows / 2;
         }
       }
     }
   } /* end of parallel section */
 
   if (verbose) cerr << lightgrey << "remapping ... " << reset;
+  remap( blank, mask, map_x, map_y, CV_INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0, 0) );
   remap( work_plane, dst, map_x, map_y, CV_INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0, 0) );
 
   /// Compute difference
   if (verbose) cerr << lightgrey << "computing difference ... " << reset;
   unsigned long num = 0;
   unsigned long quot = 0;
-#pragma omp parallel shared(dst, ref_image) private(i, j, pa, pb) reduction(+:num,quot)
+#pragma omp parallel shared(dst, ref_plane) private(i, j, pa, pb) reduction(+:num,quot)
   {
 #pragma omp for collapse(2)
     for ( j = 0; j < work_plane.rows; j++ ) {
       for ( i = 0; i < work_plane.cols; i++ ) {
-        if (dst.at<ushort>(i, j) > 0 and ref_image.at<ushort>(i,j) > 0) { // select unmasked
+        if (dst.at<ushort>(i, j) > 0 and ref_plane.at<ushort>(i,j) > 0) { // select unmasked
           pa = dst.at<ushort>(j, i);
-          pb = ref_image.at<ushort>(j,i);
+          pb = ref_plane.at<ushort>(j,i);
           num += abs((double)pa - (double)pb);
           quot += 1;
         }
@@ -127,12 +170,6 @@ double diff (double a, double b, double c, double d) {
   }
 
   return tca;
-}
-
-string to_string(double v) {
-  stringstream ss;
-  ss << v;
-  return ss.str();
 }
 
 // --------------------------------------------------------------------
@@ -176,7 +213,7 @@ void run_survey (struct argp_state* state) {
 
   // Load the images
   cerr << lightgrey << "loading reference plane " << reset << args.ref_file << "\r" << flush;
-  ref_image = imread( args.ref_file, CV_LOAD_IMAGE_ANYDEPTH ); // grayscale
+  ref_plane = imread( args.ref_file, CV_LOAD_IMAGE_ANYDEPTH ); // grayscale
   cerr << setw(100) << left << 0 << "\r" << flush; // blank line
   cerr << lightgrey << "reference plane: " << reset << lightgreen << args.ref_file << reset << endl;
 
@@ -184,12 +221,33 @@ void run_survey (struct argp_state* state) {
   work_plane = imread( args.work_plane_file, CV_LOAD_IMAGE_ANYDEPTH );
   cerr << setw(100) << left << 0 << "\r" << flush; // blank line
 
-  if (ref_image.cols != work_plane.cols or ref_image.rows != work_plane.rows) {
+  if (ref_plane.cols != work_plane.cols or ref_plane.rows != work_plane.rows) {
     cerr << on_red << "Input images must have identical geometry. Got "
-      << bold << ref_image.rows << reset << on_red << "×" << bold << ref_image.cols << reset
+      << bold << ref_plane.rows << reset << on_red << "×" << bold << ref_plane.cols << reset
       << on_red << " and "
       << bold << work_plane.rows << reset << on_red << "×" << bold << work_plane.cols << reset
       << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  if (ref_plane.cols == CFA_WIDTH and ref_plane.rows == CFA_HEIGHT) {
+    exr_mode = false;
+  }
+  else if (
+    ref_plane.rows == CFA_WIDTH + CFA_HEIGHT and
+    ref_plane.cols == CFA_WIDTH + CFA_HEIGHT
+  ) {
+    exr_mode = true;
+  }
+  else {
+    cerr << on_red << "Unrecognized image geometry: "
+      << bold << ref_plane.rows << reset << on_red << "×" << bold << ref_plane.cols << reset
+      << endl;
+    cerr << lightgrey << "Expected image formats are either " << reset
+      << bold << (CFA_WIDTH + CFA_HEIGHT) << reset << "×" << bold << (CFA_WIDTH + CFA_HEIGHT) << reset
+      << lightgrey << " (rotated high-resolution EXR matrix) or "
+      << bold << CFA_WIDTH << reset << "×" << bold << CFA_HEIGHT << reset
+      << lightgrey << " (a single EXR subframe)" << reset << endl;
     exit(EXIT_FAILURE);
   }
 
