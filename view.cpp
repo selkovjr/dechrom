31f7ca75a6091ebacd09dddbd5d37d1a4755e597
Gene Selkov <selkovjr@gmail.com>
2016-07-30 13:10:40 -0700

    added offset options to view

--- view.cpp
+++ view.cpp
@@ -58,42 +58,42 @@ void run_view (struct argp_state* state) {
     }
 
     Mat out(3 * size + 4 * padding, 3 * size + 4 * padding, input.type());
-    out.setTo(Scalar(0, 0, 0));
+    out.setTo(Scalar(20000, 20000, 20000));
 
-    // Center
-    Mat(input, Rect(width / 2 - size / 2, height / 2 - size / 2, size, size))
+    // Center (0)
+    Mat(input, Rect(width / 2 - size / 2 + args.center_x, height / 2 - size / 2 + args.center_y, size, size))
       .copyTo(Mat(out, Rect(size + 2 * padding, size + 2 * padding, size, size)));
 
-    // North
-    Mat(input, Rect((int)(cfaWidth / 2 - size / 4), (int)(cfaHeight / 2 - size / 4), size, size))
+    // North (1)
+    Mat(input, Rect(cfaWidth / 2 - size / 4 + args.n_x, cfaHeight / 2 - size / 4 + args.n_y, size, size))
       .copyTo(Mat(out, Rect(size + 2 * padding, padding, size, size)));
 
-    // Northeast
-    Mat(input, Rect((int)(cfaWidth - size / 2), 0, size, size))
+    // Northeast (2)
+    Mat(input, Rect(cfaWidth - size / 2 + args.ne_x, 0 + args.ne_y, size, size))
       .copyTo(Mat(out, Rect(2 * size + 3 * padding, padding, size, size)));
 
-    // East
-    Mat(input, Rect((int)(cfaWidth + (cfaHeight - 1.25 * size) / 2), (int)((cfaHeight - 0.75 * size) / 2), size, size))
+    // East (3)
+    Mat(input, Rect(cfaWidth + (cfaHeight - 1.25 * size) / 2 + args.e_x, (cfaHeight - 0.75 * size) / 2 + args.e_y, size, size))
       .copyTo(Mat(out, Rect(2 * size + 3 * padding, size + 2 * padding, size, size)));
 
-    // Southeast
-    Mat(input, Rect(width - size, cfaHeight - size / 2, size, size))
+    // Southeast (4)
+    Mat(input, Rect(width - size + args.se_x, cfaHeight - size / 2 + args.se_y, size, size))
       .copyTo(Mat(out, Rect(2 * size + 3 * padding, 2 * size + 3 * padding, size, size)));
 
-    // South
-    Mat(input, Rect((int)(cfaWidth  + cfaHeight / 2 - 0.75 * size), (int)(cfaHeight + cfaWidth / 2 - 0.75 * size), size, size))
+    // South (5)
+    Mat(input, Rect(cfaWidth + cfaHeight / 2 - 0.75 * size + args.s_x, cfaHeight + cfaWidth / 2 - 0.75 * size + args.s_y, size, size))
       .copyTo(Mat(out, Rect(size + 2 * padding, 2 * size + 3 * padding, size, size)));
 
-    // Southwest
-    Mat(input, Rect(cfaHeight - size / 2, cfaWidth + cfaHeight - size, size, size))
+    // Southwest (6)
+    Mat(input, Rect(cfaHeight - size / 2 + args.sw_x, cfaWidth + cfaHeight - size + args.sw_y, size, size))
       .copyTo(Mat(out, Rect(padding, 2 * size + 3 * padding, size, size)));
 
-    // West
-    Mat(input, Rect((int)(cfaHeight / 2 - 0.25 * size), (int)(cfaWidth + (cfaHeight - 1.25 * size) / 2), size, size))
+    // West (7)
+    Mat(input, Rect(cfaHeight / 2 - 0.25 * size + args.w_x, cfaWidth + (cfaHeight - 1.25 * size) / 2 + args.w_y, size, size))
       .copyTo(Mat(out, Rect(padding, size + 2 * padding, size, size)));
 
-    // Northwest
-    Mat(input, Rect(0, (int)(cfaWidth - size / 2), size, size))
+    // Northwest (8)
+    Mat(input, Rect(0 + args.nw_x, cfaWidth - size / 2 + args.nw_y, size, size))
       .copyTo(Mat(out, Rect(padding, padding, size, size)));
 
     imwrite(args.output_file, out);
--- view_args.h
+++ view_args.h
@@ -13,11 +13,20 @@ using namespace termcolor;
 // ## view command parser
 //
 struct arg_view {
-  int size;
   char* input_file;
   char* output_file;
   bool r, g, b;
   bool exr;
+  int size;
+  int center_x, center_y;
+  int n_x, n_y;
+  int ne_x, ne_y;
+  int e_x, e_y;
+  int se_x, se_y;
+  int s_x, s_y;
+  int sw_x, sw_y;
+  int w_x, w_y;
+  int nw_x, nw_y;
 };
 
 static char args_doc_view[] = "input.tiff output.tiff";
@@ -38,7 +47,7 @@ static error_t parse_view_command(int key, char* arg, struct argp_state* state) 
   switch(key) {
     case 's':
       if (sscanf(arg, "%d", &(arguments->size)) != 1) {
-        cerr << on_red << "Expecting an integer tile size. Got this: instead" << bold << arg << reset << endl;
+        cerr << on_red << "Expecting an integer tile size. Got this instead: " << bold << arg << reset << endl;
         exit(EXIT_FAILURE);
       }
       break;
@@ -59,6 +68,69 @@ static error_t parse_view_command(int key, char* arg, struct argp_state* state) 
       arguments->exr = true;
       break;
 
+    case '0':
+      if (sscanf(arg, "%d,%d", &(arguments->center_x), &(arguments->center_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for central patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '1':
+      if (sscanf(arg, "%d,%d", &(arguments->n_x), &(arguments->n_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for north patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '2':
+      if (sscanf(arg, "%d,%d", &(arguments->ne_x), &(arguments->ne_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for northeast patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '3':
+      if (sscanf(arg, "%d,%d", &(arguments->e_x), &(arguments->e_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for east patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '4':
+      if (sscanf(arg, "%d,%d", &(arguments->se_x), &(arguments->se_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for southeast patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '5':
+      if (sscanf(arg, "%d,%d", &(arguments->s_x), &(arguments->s_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for south patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '6':
+      if (sscanf(arg, "%d,%d", &(arguments->sw_x), &(arguments->sw_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for southwest patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '7':
+      if (sscanf(arg, "%d,%d", &(arguments->w_x), &(arguments->w_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for west patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
+    case '8':
+      if (sscanf(arg, "%d,%d", &(arguments->nw_x), &(arguments->nw_y)) != 2) {
+        cerr << on_red << "Expecting integer offests for northwest patch (X,Y). Got this instead: " << bold << arg << reset << endl;
+        exit(EXIT_FAILURE);
+      }
+      break;
+
     case ARGP_KEY_NO_ARGS:
       argp_usage (state);
 
@@ -71,9 +143,7 @@ static error_t parse_view_command(int key, char* arg, struct argp_state* state) 
         cerr << on_red << "File does not exist: " << bold << arguments->input_file << reset << endl;
         exit(EXIT_FAILURE);
       }
-
       arguments->output_file = nonopt[0];
-
       break;
 
     case ARGP_KEY_END:
@@ -95,11 +165,20 @@ static error_t parse_view_command(int key, char* arg, struct argp_state* state) 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 static struct argp_option options_view[] = {
-  {"red",   'r',    0,     0,   "display red channel" },
-  {"green", 'g',    0,     0,   "display green channel" },
-  {"blue",  'b',    0,     0,   "display blue channel" },
-  {"size",  's', "NUMBER", 0,   "tile size (default 1024)" },
-  {"exr",   'x',    0,     0,   "input images is a  tilted EXR array" },
+  {"red",        'r',    0,     0,   "display red channel" },
+  {"green",      'g',    0,     0,   "display green channel" },
+  {"blue",       'b',    0,     0,   "display blue channel" },
+  {"size",       's', "NUMBER", 0,   "tile size (default 1024)" },
+  {"exr",        'x',    0,     0,   "input images is a  tilted EXR array" },
+  {"center",     '0',  "X,Y",   0,   "central patch offsets" },
+  {"north",      '1',  "X,Y",   0,   "north patch offsets" },
+  {"northeast",  '2',  "X,Y",   0,   "northeast patch offsets" },
+  {"east",       '3',  "X,Y",   0,   "east patch offsets" },
+  {"southeast",  '4',  "X,Y",   0,   "southeast patch offsets" },
+  {"south",      '5',  "X,Y",   0,   "south patch offsets" },
+  {"southwest",  '6',  "X,Y",   0,   "southwest patch offsets" },
+  {"west",       '7',  "X,Y",   0,   "west patch offsets" },
+  {"northwest",  '8',  "X,Y",   0,   "northwest patch offsets" },
   { 0 }
 };
 
@@ -124,6 +203,24 @@ static struct argp argp_view = {
   args.g = false; \
   args.b = false; \
   args.size = 1024; \
+  args.center_x = 0; \
+  args.center_y = 0; \
+  args.n_x = 0; \
+  args.n_y = 0; \
+  args.ne_x = 0; \
+  args.ne_y = 0; \
+  args.e_x = 0; \
+  args.e_y = 0; \
+  args.se_x = 0; \
+  args.se_y = 0; \
+  args.s_x = 0; \
+  args.s_y = 0; \
+  args.sw_x = 0; \
+  args.sw_y = 0; \
+  args.w_x = 0; \
+  args.w_y = 0; \
+  args.nw_x = 0; \
+  args.nw_y = 0; \
   argp_parse(&argp_view, argc, argv, ARGP_IN_ORDER, &argc, &args); \
   free(argv[0]); \
   argv[0] = argv0; \
